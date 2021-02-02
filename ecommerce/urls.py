875bf034bacced830fda7192923293d6cb9c3ddb
Felipe N Souza <fnscoder@gmail.com>
2021-02-01 22:52:01 -0300

    Added swagger documentation close #19

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,9 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce API",
+        default_version='v1',
+        description="API E-commerce",
+        terms_of_service="",
+        contact=openapi.Contact(email="ecommerce@email.com"),
+        license=openapi.License(name="Copyright"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('products.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
