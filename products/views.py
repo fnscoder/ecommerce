cdf22b84407a5d8d99399e382d9a33d303035bcf
Felipe N Souza <fnscoder@gmail.com>
2021-02-01 22:17:06 -0300

    Configured filters on products endpoint close #11

--- products/views.py
+++ products/views.py
@@ -12,6 +12,23 @@ class ProductModelViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        """
+        Optionally restricts the returned products by filtering against a `name`,
+        `min_price` and `max_price` query parameter in the URL.
+        """
+        queryset = Product.objects.all()
+        name = self.request.query_params.get('name')
+        min_price = self.request.query_params.get('min_price')
+        max_price = self.request.query_params.get('max_price')
+        if name:
+            queryset = queryset.filter(name=name)
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+        return queryset
+
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
